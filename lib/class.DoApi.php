386c707e3bb375d3a7cc6454da2f7e6a8dbbacb7
Mandoo <labs@mandoocms.com>
2013-04-24 00:19:50 +0200

    Change to new class.DoApi.php from Sourceforge

--- lib/class.DoApi.php
+++ lib/class.DoApi.php
@@ -1,25 +1,29 @@
 <?php
 	/*
-	* 2012 Mandoo
-	*
-	* NOTICE OF LICENSE
-	* This source file is subject to the Open Software License (OSL 3.0)
-	* that is bundled with this package in the file LICENSE.txt.
-	* It is also available through the world-wide-web at this URL:
-	* http://opensource.org/licenses/osl-3.0.php
-	* If you did not receive a copy of the license and are unable to
-	* obtain it through the world-wide-web, please send an email
-	* to license@mandoocms.com so we can send you a copy immediately.
-	*
-	* DISCLAIMER
-	* Do not edit or add to this file if you wish to upgrade this file to newer
-	* versions in the future
-	*
-	*  @author Mandoo Team <help@mandoocms.com>
-	*  @copyright  2012 Mandoo
-	*  @version  Release: $Revision: 2532 $
-	*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-	*  International Registered Trademark & Property of Global Mandoo SL
+		Copyright (c) 2013 Global Mandoo SL
+
+		Permission is hereby granted, free of charge, to any person obtaining a 
+		copy of this software and associated documentation files (the "Software"), 
+		to deal in the Software without restriction, including without limitation 
+		the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+		and/or sell copies of the Software, and to permit persons to whom the Software 
+		is furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be 
+		included in all copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+		EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+		OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+		NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+		BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+		ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+		CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+		
+		@author Mandoo Team <help@mandoocms.com>
+		@copyright  2012 Global Mandoo SL
+		@version  Release: $Revision: 2532 $
+		International Registered Trademark & Property of Global Mandoo SL
 	*/
 	class DoAPI
 	{
